c413e35b66b25e345be142491f0d9ed300c86fdb
Nathan Lawrence <nathan@medium.com>
2015-08-25 15:23:44 -0700

    Upgrade to use Nan>=2.0

--- src/oidNative.cc
+++ src/oidNative.cc
@@ -7,37 +7,34 @@
 using namespace v8;
 
 NAN_METHOD(ObjectIdHash) {
-    NanScope();
-
-    Local<Value> val = args[0];
+    Local<Value> val = info[0];
     if (val->IsNull()) {
-        NanReturnValue(NanNew<Integer>(99961)); // A prime number.
+        info.GetReturnValue().Set(Nan::New<Integer>(99961)); // A prime number.
+
+    } else {
+        Local<Object> obj = val->ToObject();
+        if (obj.IsEmpty()) {
+            return Nan::ThrowError("Not an object.");
+        }
+
+        int hash = obj->GetIdentityHash() & 0x7fffffff;
+
+        if (hash == 0) {
+            // V8 guarantees the original hash is non-zero, but it doesn't
+            // guarantee that it's not MININT. We want to guarantee a positive
+            // number (that is non-zero and non-negative), and so we have to
+            // deal with this case specially.
+            hash = 1;
+        }
+
+        info.GetReturnValue().Set(Nan::New<Integer>(hash));
     }
-
-    Local<Object> obj = val->ToObject();
-    if (obj.IsEmpty()) {
-        return NanThrowError("Not an object.");
-    }
-
-    int hash = obj->GetIdentityHash() & 0x7fffffff;
-
-    if (hash == 0) {
-        // V8 guarantees the original hash is non-zero, but it doesn't
-        // guarantee that it's not MININT. We want to guarantee a positive
-        // number (that is non-zero and non-negative), and so we have to
-        // deal with this case specially.
-        hash = 1;
-    }
-
-    NanReturnValue(NanNew<Integer>(hash));
 }
 
 NAN_METHOD(NumberIdHash) {
-    NanScope();
-
-    Local<Number> num = args[0]->ToNumber();
+    Local<Number> num = info[0]->ToNumber();
     if (num.IsEmpty()) {
-        return NanThrowError("Not a number.");
+        return Nan::ThrowError("Not a number.");
     }
 
     union {
@@ -59,14 +56,14 @@ NAN_METHOD(NumberIdHash) {
         hash = 1;
     }
 
-    NanReturnValue(NanNew<Number>((double) hash));
+    info.GetReturnValue().Set(Nan::New<Number>((double) hash));
 }
 
 void init(Handle<Object> target) {
-    target->Set(NanNew<String>("objectIdHash"),
-                NanNew<FunctionTemplate>(ObjectIdHash)->GetFunction());
-    target->Set(NanNew<String>("numberIdHash"),
-                NanNew<FunctionTemplate>(NumberIdHash)->GetFunction());
+    target->Set(Nan::New<String>("objectIdHash").ToLocalChecked(),
+                Nan::New<FunctionTemplate>(ObjectIdHash)->GetFunction());
+    target->Set(Nan::New<String>("numberIdHash").ToLocalChecked(),
+                Nan::New<FunctionTemplate>(NumberIdHash)->GetFunction());
 }
 
 NODE_MODULE(oidNative, init)
